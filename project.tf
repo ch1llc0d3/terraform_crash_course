99af96b6c91c30a6e3cf3d8dc70bb92f14793e1e
ch1llc0d3 <chillcrib7@gmail.com>
2024-02-16 16:55:46 -0300

    adding changes, output and stuff

--- project.tf
+++ project.tf
@@ -166,7 +166,9 @@ resource "aws_eip" "one" {
 #   network_interface         = aws_network_interface.multi-ip.id
     network_interface = aws_network_interface.web-server-cece.id
     # associate_with_private_ip = "10.0.0.10"
-    associate_with_private_ip = "10.0.1.85" 
+    # check it out because this public ip always changes 
+    # https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1
+    associate_with_private_ip = "10.0.1.205" 
     # If you try to create an EIP, and assing it to a device that's 
 #     on a subnet or in a VPC that doesn't have an IGW (internet gate way),
 #     it will throw an error 
@@ -177,6 +179,10 @@ resource "aws_eip" "one" {
     # depends_on = aws_internet_gateway.gw
     # have to pass it on as list so the [ ] is needed
     depends_on = [ aws_internet_gateway.gw ]
+}
+
+output "server_public_ip" {
+  value = aws_eip.one.public_ip
 }
 
 # 9. Create Ubuntu server and install/enable apache2
@@ -211,4 +217,13 @@ resource "aws_instance" "web-server-instance" {
         Name = "web server"
     }
 
+}
+
+output "server_private_ip" {
+  value = aws_instance.web-server-instance.private_ip
+  
+}
+
+output "server_id" {
+    value = aws_instance.web-server-instance.id
 }
